91075c5e66452a2ab545a8527be92bf4b84ff2cc
shoyaa <daioukuten@gmail.com>
2023-04-05 13:03:12 +0300

    added nprogress for page transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import store from "../store";
-//TEST
+import NextNProgress from "nextjs-progressbar";
+
 const montserrat = Montserrat({
   subsets: ["latin"],
 
@@ -20,6 +21,7 @@ export default function App({
 }: AppProps<{ session: Session }>) {
   return (
     <ThemeProvider attribute="class">
+      <NextNProgress color="#0ea5e9" />
       <SessionProvider session={session}>
         <Provider store={store}>
           <main className={`${montserrat.variable} font-sans`}>
